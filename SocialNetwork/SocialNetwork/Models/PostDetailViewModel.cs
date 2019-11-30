6cd34e62982d705b9846b50fd65e27e21ac70b23
Jazz1464 <v.vl.stepanenko@gmail.com>
2019-11-30 23:51:44 +0700

    Добавлена страница записи сообщества и комментарии к записи

---
+++ SocialNetwork/SocialNetwork/Models/PostDetailViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Models
+{
+    public class PostDetailViewModel
+    {
+        public PostInfo Post { get; set; }
+
+        public List<CommentInfo> Comments { get; set; }
+
+        public Comment comment { get; set; }
+
+        public bool isAuthor { get; set; }
+        public bool isAdmin { get; set; }
+
+        public Guid AuthorId { get; set; }
+    }
+}
