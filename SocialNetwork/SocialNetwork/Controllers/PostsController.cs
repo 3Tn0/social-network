76c9c63aacd6ca09fe60ee32a936116bb688fcca
Jazz1464 <v.vl.stepanenko@gmail.com>
2019-11-30 21:38:05 +0700

    Добавлена страница создания новой записи сообщества

---
+++ SocialNetwork/SocialNetwork/Controllers/PostsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Controllers
+{
+    public class PostsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Posts
+        public ActionResult Index()
+        {
+            return View(db.Posts.ToList());
+        }
+
+        // GET: Posts/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public ActionResult Create(Guid ComId)
+        {
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PostId,CreationDate,Text")] Post post, Guid ComId)
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+
+            if (ModelState.IsValid)
+            {
+                post.PostId = Guid.NewGuid();
+                post.CommunityId = ComId;
+                post.CreationDate = DateTime.Now;
+                post.RightsId = (from R in db.Editors
+                                 where R.CommunityId == ComId && R.UserId == userId && R.CancellationDate == null
+                                 select new
+                                 {
+                                     RId = R.EditorRightsId
+                                 }).First().RId;
+                db.Posts.Add(post);
+                db.SaveChanges();
+
+                return RedirectToAction("Community", "Communities", new { ComId });
+            }
+
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PostId,CreationDate,Text,RightsId,CommunityId")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Post post = db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
